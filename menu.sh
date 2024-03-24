8dd912824113893271aa1b20cc8afb17449b3a2c
Salarsdg <118529728+Salarsdg@users.noreply.github.com>
2024-03-24 17:30:48 +0330

    first change

---
+++ menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo hello this is my first script
+read -p "Enter option number: " choice
+echo 1. update and upgrade the server
+
+
+
+
+
+
+
+case $choice in  
+
+1) 
+echo "you choose 1"
+
+esac
