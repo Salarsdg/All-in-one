154c49784ebd34060c63a1c0797bd0bf84907f89
Salarsdg <118529728+Salarsdg@users.noreply.github.com>
2024-03-24 17:59:24 +0330

    ..

--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 echo hello this is my first script
-echo 1. update and upgrade the server
+echo 1. update and install needed packages.
 echo 2. install telegram bot.
 read -p "Enter option number: " choice
 
@@ -19,10 +19,7 @@ read -p "Enter option number: " choice
 case $choice in  
 
 1) 
-echo -e "${YELLOW} starting the update and upgrade process.... ${NC}"
-apt update && apt upgrade -y
-
-echo -e "${GREEN} your Vps updated!!!! ${NC}"
+bash <(curl -Ls https://raw.githubusercontent.com/Salarsdg/All-in-one/main/update.sh)
 ;;
 2) 
 
--- update.sh
+++ update.sh
@@ -7,5 +7,15 @@ NC='\033[0m' # No Color
 
 echo -e "${YELLOW} starting the update and upgrade process.... ${NC}"
 apt update && apt upgrade -y
+clear
 
 echo -e "${GREEN} your Vps updated!!!! ${NC}"
+
+
+echo -e "${YELLOW} installing packages.... ${NC}"
+apt-get install -y software-properties-common ufw wget curl git socat cron busybox bash-completion locales nano apt-utils 
+
+
+echo -e "${GREEN} all packages installed. ${NC}"
+
+
