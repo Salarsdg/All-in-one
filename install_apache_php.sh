f3c5d4228f239396a68c02eddd57c723b1217567
Salarsdg <118529728+Salarsdg@users.noreply.github.com>
2024-05-01 17:47:58 +0330

    test2

--- install_apache_php.sh
+++ install_apache_php.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "hi"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW} starting the update and upgrade process.... ${NC}"
+apt update && apt upgrade -y
+clear
+
+echo -e "${GREEN} your Vps updated!!!! ${NC}"
+echo -e "${YELLOW} installing packages.... ${NC}"
+sleep 10
+
+apt-get install -y software-properties-common ufw wget curl git socat cron busybox bash-completion locales nano apt-utils certbot 
+
+
+echo -e "${GREEN} all packages installed. ${NC}"
+
+bash <(curl -Ls https://raw.githubusercontent.com/Salarsdg/All-in-one/main/menu.sh)
+
