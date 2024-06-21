2cfdb9ac2b806df2d9ce6ae783ce907b738d51cb
Salarsdg <118529728+Salarsdg@users.noreply.github.com>
2024-06-21 12:42:20 +0330

    local v1

--- ip-local-menu.sh
+++ ip-local-menu.sh
@@ -4,4 +4,26 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
+echo dar kodam server hastid ? 
+echo 1. iran
+echo 2. kharej
+read -p "Enter your choice: " choice
 
+
+
+
+
+
+
+
+case $choice in  
+
+1) 
+bash <(curl -Ls https://raw.githubusercontent.com/Salarsdg/All-in-one/main/iran.sh)
+;;
+2)  
+bash <(curl -Ls https://raw.githubusercontent.com/Salarsdg/All-in-one/main/kharej.sh)
+;;
+
+
+esac
